93b70d5e58de63a86839cf18905ce7996ac2e506
sssssqew <sssssqew@naver.com>
2024-09-14 18:25:21 +0900

    확장성을 위한 코드 리팩토링

--- script.js
+++ script.js
@@ -14,24 +14,23 @@ function lerp(start, end, t){
 }
 
 // animation functions
-function animateImage(current){
-    let scale = Math.abs(current) < 0.1 ? 0.1 : Math.abs(current) // 0.1 ~ 1
-    let rotate = (1 + current) * -240 // -240 ~ 0 // Math.floor 를 적용하면 이미지가 회전할때 뚝뚝 끊기면서 떨림현상이 발생함
+function animateImage(){
+    let portion = getScrollPortion(heroSection)
+    cPortions.hero = lerp(cPortions.hero, portion, 0.1)
+    let scale = Math.abs(cPortions.hero) < 0.1 ? 0.1 : Math.abs(cPortions.hero) // 0.1 ~ 1
+    let rotate = (1 + cPortions.hero) * -240 // -240 ~ 0 // Math.floor 를 적용하면 이미지가 회전할때 뚝뚝 끊기면서 떨림현상이 발생함
     image.style.transform = `scale(${scale}) rotate(${rotate}deg)`
 }
 function animate(){
-    current = lerp(current, portion, 0.1)
-    animateImage(current)
+    animateImage()
     requestAnimationFrame(animate)
 }
 
 const heroSection = document.getElementById('hero')
 const image = document.querySelector('img')
-let current = 0
-let portion = 0 // target
 
-window.addEventListener('scroll', () => {
-    portion = getScrollPortion(heroSection)
-})
+const cPortions = { // 추후 확장
+    hero: 0, // current position of heroSection 
+}
 
 animate()
