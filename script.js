ab05d9a1a0ec0bf8e5789a8812eef747f1dad19b
sssssqew <sssssqew@naver.com>
2024-09-14 18:01:03 +0900

    선형보간법을 이용하여 스크롤시 부드러운 애니메이션 구현하기

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// utils functions 
 function getScrollPortion(element){
     let portion = 0
     let {top} = element.getBoundingClientRect()
@@ -8,13 +9,29 @@ function getScrollPortion(element){
     return portion
 }
 
+function lerp(start, end, t){
+    return start * (1 - t) + end * t
+}
+
+// animation functions
+function animateImage(current){
+    let scale = Math.abs(current) < 0.1 ? 0.1 : Math.abs(current) // 0.1 ~ 1
+    let rotate = (1 + current) * -240 // -240 ~ 0 // Math.floor 를 적용하면 이미지가 회전할때 뚝뚝 끊기면서 떨림현상이 발생함
+    image.style.transform = `scale(${scale}) rotate(${rotate}deg)`
+}
+function animate(){
+    current = lerp(current, portion, 0.1)
+    animateImage(current)
+    requestAnimationFrame(animate)
+}
+
 const heroSection = document.getElementById('hero')
 const image = document.querySelector('img')
+let current = 0
+let portion = 0 // target
 
 window.addEventListener('scroll', () => {
-    let portion = getScrollPortion(heroSection)
-    let scale = Math.abs(portion) < 0.1 ? 0.1 : Math.abs(portion) // 0.1 ~ 1
-    let rotate = Math.floor((1 + portion) * -240) // -240 ~ 0
-
-    image.style.transform = `scale(${scale}) rotate(${rotate}deg)`
+    portion = getScrollPortion(heroSection)
 })
+
+animate()
