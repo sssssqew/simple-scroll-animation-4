ac96bae8c3d64afc1aba674ebd8036c659698f39
sssssqew <sssssqew@naver.com>
2024-09-21 20:47:08 +0900

    모바일 지원을 위한 단위를 svh, vvw 로 다시 변경

--- script.js
+++ script.js
@@ -156,7 +156,7 @@ function animateSlider(){
     currentPos.projects = lerp(currentPos.projects, target, 0.05) // 0.05 : the less, the smoother
     let translateX = currentPos.projects * converPxToViewport(getScrollAmount(projectSection, enableScrollBarWidth)) 
     
-    slider.style.transform = `translateX(${translateX}lvw)`
+    slider.style.transform = `translateX(${translateX}svw)`
     animateSlideImgs(target, currentPos.projects)
 }
 function animateIdentity(){
