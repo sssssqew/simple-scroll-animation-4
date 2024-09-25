7d0412db9f995d920eacf05b87eacf907cc6a193
sssssqew <sssssqew@naver.com>
2024-09-25 18:37:23 +0900

    슬라이드 애니메이션 성능 테스트 6

--- script.js
+++ script.js
@@ -176,9 +176,9 @@ async function animateAbout(){
     }
 }
 function animateSlideImgs(target, current){
-    let skewDiff = (target - current) * 50
+    let skewDiff = ((target - current) * 50).toFixed(1) 
     slideImgs.forEach((slideImg, idx) => {
-        let left = (1 + current) * elementInfos.projects.slideRangeOfImg // -30 ~ 0 / use toFixed function to enhance performance
+        let left = ((1 + current) * elementInfos.projects.slideRangeOfImg).toFixed(1) // -30 ~ 0 / use toFixed function to enhance performance
         slideImg.style.left = `${left}px`
         slideImg.parentElement.style.transform = `skewX(${skewDiff}deg)`
     })
